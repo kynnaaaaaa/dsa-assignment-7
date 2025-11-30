67916e3b3a000a0aa4b49f44d35701d2a3ed2dac
kynnaaaaaa <kynasood2006@gmail.com>
2025-11-30 17:03:41 +0530

    q3.cpp

---
+++ q3.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> nums = {2, 3, 2, 4, 3, 2};
+    unordered_map<int,int> freq;
+
+    for(int x : nums)
+        freq[x]++;
+
+    for(auto &p : freq)
+        cout << p.first << " → " << p.second << " times" << endl;
+}
