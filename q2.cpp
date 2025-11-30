64e9a2616a5b8e979a41797d853d4f877d93fc54
kynnaaaaaa <kynasood2006@gmail.com>
2025-11-30 17:01:08 +0530

    q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -1,1 +1,22 @@
-return 0;
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> commonElements(vector<int>& A, vector<int>& B) {
+    unordered_set<int> s(A.begin(), A.end());
+    vector<int> ans;
+
+    for(int x : B)
+        if(s.count(x))
+            ans.push_back(x);
+
+    return ans;
+}
+
+int main() {
+    vector<int> A = {1, 2, 3, 4};
+    vector<int> B = {3, 4, 5, 6};
+
+    vector<int> ans = commonElements(A, B);
+    for(int x : ans) cout << x << " ";
+}
+
